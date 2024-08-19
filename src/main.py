5989e49d80a7e70aeb243f9f6114903a38285a7e
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2024-08-19 23:59:42 +0500

    Add redirect from "/" to "Program Overview"

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.routing import Route
 
 from admin import admin
 
-app = Starlette()
+
+async def home_root(request: Request):
+    return RedirectResponse(url="/program-pg-view/list")
+
+
+routes = [Route("/", endpoint=home_root)]
+
+app = Starlette(routes=routes)
 admin.mount_to(app)
 
 if __name__ == "__main__":
